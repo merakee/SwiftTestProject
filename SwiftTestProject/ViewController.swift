f63fd5749afcc01946475e178b7b84eada1daf8d
Freelogue <freelogue@users.noreply.github.com>
2015-02-24 16:04:15 -0800

    Initial Commit

---
+++ SwiftTestProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftTestProject
+//
+//  Created by Bijit Halder on 2/24/15.
+//  Copyright (c) 2015 Bijit Halder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
